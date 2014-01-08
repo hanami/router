77bbd043c9716511b9bc30b292a56f252842db45
Luca Guidi <me@lucaguidi.com>
2014-01-08 09:46:07 +0100

    Initial version 0.1.0

--- lib/lotus/router/version.rb
+++ lib/lotus/router/version.rb
@@ -3,6 +3,6 @@ end unless defined?(HttpRouter)
 
 module Lotus
   class Router < HttpRouter
-    VERSION = '0.0.1'
+    VERSION = '0.1.0'
   end
 end
--- test/version_test.rb
+++ test/version_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 describe Lotus::Router::VERSION do
-  it 'is 0.0.1' do
-    Lotus::Router::VERSION.must_equal '0.0.1'
+  it 'exposes version' do
+    Lotus::Router::VERSION.must_equal '0.1.0'
   end
 end
