e03a0c6db7386fc468a8750e65a014398f08b2b3
Luca Guidi <me@lucaguidi.com>
2014-01-09 11:05:20 +0100

    Documentation for Lotus::Routing::EndpointResolver

--- lib/lotus/routing/endpoint_resolver.rb
+++ lib/lotus/routing/endpoint_resolver.rb
@@ -4,10 +4,124 @@ require 'lotus/routing/endpoint'
 
 module Lotus
   module Routing
+    # Resolve duck-typed endpoints
+    #
+    # @since 0.1.0
+    #
+    # @api private
     class EndpointResolver
+      # Default suffix appended to the controller#action string.
+      # A different suffix can be passed to #initialize with the `:suffix` option.
+      #
+      # @see #initialize
+      # @see #resolve
+      #
+      # @since 0.1.0
+      #
+      # @example
+      #   require 'lotus/router'
+      #
+      #   router = Lotus::Router.new do
+      #     get '/', to: 'articles#show'
+      #   end
+      #
+      #   # That string is transformed into "Articles(::Controller::|Controller::)Show"
+      #   # because the risolver is able to lookup (in the given order) for:
+      #   #
+      #   #  * Articles::Controller::Show
+      #   #  * ArticlesController::Show
+      #   #
+      #   # When it finds a class, it stops the lookup and returns the result.
       SUFFIX = '(::Controller::|Controller::)'.freeze
+
+      # Default separator for controller and action.
+      # A different separator can be passed to #initialize with the `:separator` option.
+      #
+      # @see #initialize
+      # @see #resolve
+      #
+      # @since 0.1.0
+      #
+      # @example
+      #   require 'lotus/router'
+      #
+      #   router = Lotus::Router.new do
+      #     get '/', to: 'articles#show'
+      #   end
       ACTION_SEPARATOR = /#/.freeze
 
+      # Initialize an endpoint resolver
+      #
+      # @param options [Hash] the options used to customize lookup behavior
+      #
+      # @option options [Class] :endpoint the endpoint class that is returned
+      #   by `#resolve`. (defaults to `Lotus::Routing::Endpoint`)
+      #
+      # @option options [Class,Module] :namespace the Ruby namespace where to
+      #   lookup for controllers and actions. (defaults to `Object`)
+      #
+      # @option options [String] :suffix the suffix appended to the controller
+      #   name during the lookup. (defaults to `SUFFIX`)
+      #
+      # @option options [String] :separator the sepatator between controller and
+      #   action name. (defaults to `ACTION_SEPARATOR`)
+      #
+      #
+      #
+      # @return [Lotus::Routing::EndpointResolver] self
+      #
+      #
+      #
+      # @since 0.1.0
+      #
+      #
+      #
+      # @example Specify custom endpoint class
+      #   require 'lotus/router'
+      #
+      #   resolver = Lotus::Routing::EndpointResolver.new(endpoint: CustomEndpoint)
+      #   router   = Lotus::Router.new(resolver: resolver)
+      #
+      #   router.get('/', to: endpoint).dest # => #<CustomEndpoint:0x007f97f3359570 ...>
+      #
+      #
+      #
+      # @example Specify custom Ruby namespace
+      #   require 'lotus/router'
+      #
+      #   resolver = Lotus::Routing::EndpointResolver.new(namespace: MyApp)
+      #   router   = Lotus::Router.new(resolver: resolver)
+      #
+      #   router.get('/', to: 'articles#show')
+      #   # => Will look for:
+      #   #  * MyApp::Articles::Controller::Show
+      #   #  * MyApp::ArticlesController::Show
+      #
+      #
+      #
+      # @example Specify custom controller suffix
+      #   require 'lotus/router'
+      #
+      #   resolver = Lotus::Routing::EndpointResolver.new(suffix: '(Controller::|Ctrl::)')
+      #   router   = Lotus::Router.new(resolver: resolver)
+      #
+      #   router.get('/', to: 'articles#show')
+      #   # => Will look for:
+      #   #  * ArticlesController::Show
+      #   #  * ArticlesCtrl::Show
+      #
+      #
+      #
+      # @example Specify custom controller-action separator
+      #   require 'lotus/router'
+      #
+      #   resolver = Lotus::Routing::EndpointResolver.new(separator: '@')
+      #   router   = Lotus::Router.new(resolver: resolver)
+      #
+      #   router.get('/', to: 'articles@show')
+      #   # => Will look for:
+      #   #  * Articles::Controller::Show
+      #   #  * ArticlesController::Show
       def initialize(options = {})
         @endpoint_class = options[:endpoint]  || Endpoint
         @namespace      = options[:namespace] || Object
@@ -15,12 +129,72 @@ module Lotus
         @separator      = options[:separator] || ACTION_SEPARATOR
       end
 
+      # Resolve the given set of HTTP verb, path, endpoint and options.
+      #
+      # @param options [Hash] the options required to resolve the endpoint
+      #
+      # @option options [String,Proc,Class,Object#call] :to the endpoint
+      # @option options [String] :prefix an optional path prefix
+      #
+      # @return [Endpoint] this may vary according to the :endpoint option
+      #   passed to #initialize
+      #
+      # @since 0.1.0
+      #
+      # @see #initialize
+      # @see #find
+      #
+      # @example Resolve to a Proc
+      #   require 'lotus/router'
+      #
+      #   router = Lotus::Router.new
+      #   router.get '/', to: ->(env) { [200, {}, ['Hi!']] }
+      #
+      # @example Resolve to a class
+      #   require 'lotus/router'
+      #
+      #   router = Lotus::Router.new
+      #   router.get '/', to: RackMiddleware
+      #
+      # @example Resolve to a Rack compatible object (respond to #call)
+      #   require 'lotus/router'
+      #
+      #   router = Lotus::Router.new
+      #   router.get '/', to: AnotherMiddleware.new
+      #
+      # @example Resolve to a Lotus::Action from a string (see Lotus::Controller framework)
+      #   require 'lotus/router'
+      #
+      #   router = Lotus::Router.new
+      #   router.get '/', to: 'articles#show'
+      #
+      # @example Resolve to a Lotus::Action (see Lotus::Controller framework)
+      #   require 'lotus/router'
+      #
+      #   router = Lotus::Router.new
+      #   router.get '/', to: ArticlesController::Show
+      #
+      # @example Resolve with a path prefix
+      #   require 'lotus/router'
+      #
+      #   router = Lotus::Router.new
+      #   router.get '/dashboard', to: BackendApp.new, prefix: 'backend'
+      #     # => Will be available under '/backend/dashboard'
       def resolve(options, &endpoint)
         result = endpoint || find(options)
         resolve_callable(result) || resolve_matchable(result) || default
       end
 
-      def find(options, &endpoint)
+      # Finds a path from the given options.
+      #
+      # @param options [Hash] the path description
+      # @option options [String,Proc,Class,Object#call] :to the endpoint
+      # @option options [String] :prefix an optional path prefix
+      #
+      # @since 0.1.0
+      #
+      # @return [Object]
+      def find(options)
         if prefix = options[:prefix]
           prefix.join(options[:to])
         else
