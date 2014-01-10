119d20eff268543cc3f5bcd712f2632f59493f8c
Luca Guidi <me@lucaguidi.com>
2014-01-10 11:06:52 +0100

    Documentation for Lotus::Routing::Namespace

--- lib/lotus/routing/namespace.rb
+++ lib/lotus/routing/namespace.rb
@@ -3,7 +3,17 @@ require 'lotus/utils/path_prefix'
 
 module Lotus
   module Routing
+    # Namespace for routes.
+    # Implementation of Lotus::Router#namespace
+    #
+    # @since 0.1.0
+    #
+    # @api private
+    #
+    # @see Lotus::Router#namespace
     class Namespace < SimpleDelegator
+      # @api private
+      # @since 0.1.0
       def initialize(router, name, &blk)
         @router = router
         @name   = Utils::PathPrefix.new(name)
@@ -11,42 +21,63 @@ module Lotus
         instance_eval(&blk)
       end
 
+      # @api private
+      # @since 0.1.0
       def get(path, options = {}, &endpoint)
         super(@name.join(path), options, &endpoint)
       end
 
+      # @api private
+      # @since 0.1.0
       def post(path, options = {}, &endpoint)
         super(@name.join(path), options, &endpoint)
       end
 
+      # @api private
+      # @since 0.1.0
       def put(path, options = {}, &endpoint)
         super(@name.join(path), options, &endpoint)
       end
 
+      # @api private
+      # @since 0.1.0
       def patch(path, options = {}, &endpoint)
         super(@name.join(path), options, &endpoint)
       end
 
+      # @api private
+      # @since 0.1.0
       def delete(path, options = {}, &endpoint)
         super(@name.join(path), options, &endpoint)
       end
 
+      # @api private
+      # @since 0.1.0
       def trace(path, options = {}, &endpoint)
         super(@name.join(path), options, &endpoint)
       end
 
+      # @api private
+      # @since 0.1.0
       def resource(name, options = {})
         super name, options.merge(prefix: @name)
       end
 
+      # @api private
+      # @since 0.1.0
       def resources(name, options = {})
         super name, options.merge(prefix: @name)
       end
 
+      # @api private
+      # @since 0.1.0
       def redirect(path, options = {}, &endpoint)
         super(@name.join(path), options.merge(prefix: @name), &endpoint)
       end
 
+      # Supports nested namespaces
+      # @api private
+      # @since 0.1.0
       def namespace(name, &blk)
         Routing::Namespace.new(self, name, &blk)
       end
