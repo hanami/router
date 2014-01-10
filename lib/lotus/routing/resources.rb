b76bd6fc180e260b5deb2523e55f21bad4043642
Luca Guidi <me@lucaguidi.com>
2014-01-10 15:36:56 +0100

    Documentation for Lotus::Routing::Resources

--- lib/lotus/routing/resources.rb
+++ lib/lotus/routing/resources.rb
@@ -3,10 +3,37 @@ require 'lotus/routing/resources/action'
 
 module Lotus
   module Routing
+    # Set of RESTful resources routes
+    # Implementation of Lotus::Router#resources
+    #
+    # @since 0.1.0
+    #
+    # @api private
+    #
+    # @see Lotus::Router#resources
     class Resources < Resource
-      self.actions    = [:index, :new, :create, :show, :edit, :update, :destroy]
-      self.action     = Resources::Action
-      self.member     = Resources::MemberAction
+      # Set of default routes
+      #
+      # @api private
+      # @since 0.1.0
+      self.actions = [:index, :new, :create, :show, :edit, :update, :destroy]
+
+      # Action class
+      #
+      # @api private
+      # @since 0.1.0
+      self.action = Resources::Action
+
+      # Member action class
+      #
+      # @api private
+      # @since 0.1.0
+      self.member = Resources::MemberAction
+
+      # Collection action class
+      #
+      # @api private
+      # @since 0.1.0
       self.collection = Resources::CollectionAction
     end
   end
