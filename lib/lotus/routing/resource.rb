151d5a34b020fd2907ac500ce91125d869c6fdfc
Luca Guidi <me@lucaguidi.com>
2014-01-10 15:35:05 +0100

    Documentation for Lotus::Routing::Resource

--- lib/lotus/routing/resource.rb
+++ lib/lotus/routing/resource.rb
@@ -4,21 +4,47 @@ require 'lotus/routing/resource/action'
 
 module Lotus
   module Routing
+    # Set of RESTful resource routes
+    # Implementation of Lotus::Router#resource
+    #
+    # @since 0.1.0
+    #
+    # @api private
+    #
+    # @see Lotus::Router#resource
     class Resource
       include Utils::ClassAttribute
 
+      # Set of default routes
+      #
+      # @api private
+      # @since 0.1.0
       class_attribute :actions
       self.actions = [:new, :create, :show, :edit, :update, :destroy]
 
+      # Action class
+      #
+      # @api private
+      # @since 0.1.0
       class_attribute :action
       self.action = Resource::Action
 
+      # Member action class
+      #
+      # @api private
+      # @since 0.1.0
       class_attribute :member
       self.member = Resource::MemberAction
 
+      # Collection action class
+      #
+      # @api private
+      # @since 0.1.0
       class_attribute :collection
       self.collection = Resource::CollectionAction
 
+      # @api private
+      # @since 0.1.0
       def initialize(router, name, options = {}, &blk)
         @router  = router
         @name    = name
