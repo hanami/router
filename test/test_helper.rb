86e9407434827671ab662ed2d6ac7d3806cc2b17
Luca Guidi <me@lucaguidi.com>
2014-01-10 16:07:28 +0100

    Add test coverage instrumentation

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,11 @@
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter 'test'
+    command_name 'Mintest'
+  end
+end
+
 require 'rubygems'
 require 'bundler/setup'
 require 'minitest/autorun'
