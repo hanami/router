f87976e0d6b3c5378cdfc467fb8080ada8c01d86
Luca Guidi <me@lucaguidi.com>
2014-01-10 16:02:57 +0100

    Documentation for Lotus::Routing::Resource::Options

--- lib/lotus/routing/resource/options.rb
+++ lib/lotus/routing/resource/options.rb
@@ -1,9 +1,40 @@
 module Lotus
   module Routing
     class Resource
+      # Options for RESTFul resource(s)
+      #
+      # @api private
+      # @since 0.1.0
+      #
+      # @see Lotus::Router#resource
+      # @see Lotus::Router#resources
       class Options < Hash
+        # @api private
+        # @since 0.1.0
         attr_reader :actions
 
+        # Initialize the options for:
+        #   * Lotus::Router#resource
+        #   * Lotus::Router#resources
+        #
+        # @param actions [Array<Symbol>] the name of the actions
+        # @param options [Hash]
+        # @option options [Hash] :only white list of the default actions
+        # @option options [Hash] :except black list of the default actions
+        #
+        # @api private
+        # @since 0.1.0
+        #
+        # @see Lotus::Routing::Resource
+        # @see Lotus::Routing::Resources
+        #
+        # @example
+        #   require 'lotus/router'
+        #
+        #   Lotus::Router.new do
+        #     resources 'articles', only:   [:index]
+        #     resource  'profile',  except: [:new, :create, :destroy]
+        #   end
         def initialize(actions, options = {})
           only     = Array(options.delete(:only) || actions)
           except   = Array(options.delete(:except))
