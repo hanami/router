46129fd1b2bfc5e45806f71041bea0596236e138
Alfonso Uceda Pompa <alfonso@aspgems.com>
2014-12-04 17:04:50 +0100

    Added hability to inspect nested routes [resolve #34]

--- lib/lotus/routing/routes_inspector.rb
+++ lib/lotus/routing/routes_inspector.rb
@@ -23,6 +23,7 @@ module Lotus
       # Return a formatted string that describes all the routes
       #
       # @param formatter [String] the optional formatter for a route
+      # @param base_path [String] the base path of a nested route
       #
       # @return [String] routes pretty print
       #
@@ -63,17 +64,57 @@ module Lotus
       #          | GET, HEAD | login  | /login  | Sessions::New     |
       #          | POST      |        | /login  | Sessions::Create  |
       #          | GET, HEAD | logout | /logout | Sessions::Destroy |
-      def to_s(formatter = FORMATTER)
-        result = ""
-
-        @routes.each do |route|
-          result << formatter % inspect_route(route)
+      #
+      # @example Nested routes
+      #   require 'lotus/router'
+      #
+      #   class AdminLotusApp
+      #     def call(env)
+      #     end
+      #     def routes
+      #       Lotus::Router.new {
+      #         get '/home', to: 'home#index'
+      #       }
+      #     end
+      #   end
+      #
+      #   router = Lotus::Router.new {
+      #     get '/fakeroute', to: 'fake#index'
+      #     mount AdminLotusApp, at: '/admin'
+      #     mount Lotus::Router.new {
+      #       get '/posts', to: 'posts#index'
+      #       mount Lotus::Router.new {
+      #         get '/comments', to: 'comments#index'
+      #       }, at: '/second_mount'
+      #     }, at: '/api'
+      #   }
+      #
+      #   formatter = "| %{methods} | %{name} | %{path} | %{endpoint} |\n"
+      #
+      #   puts router.inspector.to_s(formatter)
+      #     # => | GET, HEAD |  | /fakeroute                 | Fake::Index     |
+      #          | GET, HEAD |  | /admin/home                | Home::Index     |
+      #          | GET, HEAD |  | /api/posts                 | Posts::Index    |
+      #          | GET, HEAD |  | /api/second_mount/comments | Comments::Index |
+      def to_s(formatter = FORMATTER, base_path = '')
+        @routes.each_with_object("") do |route, result|
+          destination = destination(route)
+          result << if destination && destination.respond_to?(:routes)
+            destination.routes.inspector.to_s(formatter, [base_path, route.path_for_generation].join)
+          else
+            formatter % inspect_route(route, base_path)
+          end
         end
-
-        result
       end
 
       private
+
+      def destination(route)
+        route.dest.__getobj__ 
+      rescue
+        nil
+      end
+
       # Return a Hash compatible with formatter
       #
       # @return [Hash] serialized route
@@ -83,11 +124,11 @@ module Lotus
       #
       # @see Lotus::Routing::RoutesInspector#FORMATTER
       # @see Lotus::Routing::RoutesInspector#to_s
-      def inspect_route(route)
+      def inspect_route(route, base_path = '')
         Hash[
           name:     route.name,
           methods:  route.request_methods.to_a.join(", "),
-          path:     route.original_path,
+          path:     [base_path, route.original_path].join,
           endpoint: route.dest.inspect
         ]
       end
