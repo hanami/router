d4b49b2bfd0c4c0a70394c24a25882579857ce1e
Luca Guidi <me@lucaguidi.com>
2015-07-03 10:04:56 +0200

    Bump v0.4.2

--- test/version_test.rb
+++ test/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Lotus::Router::VERSION do
   it 'exposes version' do
-    Lotus::Router::VERSION.must_equal '0.4.1'
+    Lotus::Router::VERSION.must_equal '0.4.2'
   end
 end
