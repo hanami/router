4058d5f02c37e1d405740908a879bc2219c4adec
Nicola Racco <nicola@nicolaracco.com>
2016-04-11 08:45:14 +0200

    host match

--- test/integration/router_container_test.rb
+++ test/integration/router_container_test.rb
@@ -13,4 +13,17 @@ describe 'Router wrapper as container' do
     response = @app.get('/back/home', lint: true)
     response.body.must_equal 'back'
   end
+
+  it 'matches against host' do
+    @router_container = Hanami::Router.new(scheme: 'https', host: 'hanami.test', port: 443) do
+      mount Front::App, at: '/front', host: 'www.hanami.test'
+      mount Back::App, at: '/front'
+    end
+
+    @app = Rack::MockRequest.new(@router_container)
+    response = @app.get('https://www.hanami.test/front/home', lint: true)
+    response.body.must_equal 'front'
+    response = @app.get('/front/home', lint: true)
+    response.body.must_equal 'back'
+  end
 end
