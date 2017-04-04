f50b462164551fff971bd47161a844a8be7b2a8d
morrme <memthings@gmail.com>
2017-04-03 19:36:31 -0500

    integration tests done

--- spec/hanami/routing/endpoint_resolver_test.rb
+++ spec/hanami/routing/endpoint_resolver_test.rb
@@ -1,6 +1,4 @@
-require 'test_helper'
-
-describe Hanami::Routing::EndpointResolver do
+RSpec.describe Hanami::Routing::EndpointResolver do
   before do
     @resolver = Hanami::Routing::EndpointResolver.new
   end
