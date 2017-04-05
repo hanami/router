f8eb6b069518c4fdeae6bd7f842b39bfacd99a1c
morrme <morrme@users.noreply.github.com>
2017-04-05 13:48:36 -0500

    Update new_spec.rb

--- spec/new_spec.rb
+++ spec/new_spec.rb
@@ -56,7 +56,7 @@ RSpec.describe Hanami::Router do
 
     it 'checks if there are defined routes' do
       router = Hanami::Router.new
-      expect(router)not_to be(:defined)
+      expect(router).not_to be(:defined)
 
       router = Hanami::Router.new { get '/', to: ->(env) { } }
       expect(router).to be(:defined)
