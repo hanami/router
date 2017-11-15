b50c5595204eb9681f55925067cec645a7fd64bb
Luca Guidi <me@lucaguidi.com>
2017-11-15 17:32:20 +0100

    API docs [ci skip]

--- lib/hanami/routing/prefix.rb
+++ lib/hanami/routing/prefix.rb
@@ -14,7 +14,7 @@ module Hanami
     # @see Hanami::Router#prefix
     class Prefix < SimpleDelegator
       # @api private
-      # @since 0.1.0
+      # @since x.x.x
       def initialize(router, path, &blk)
         @router = router
         @path   = Utils::PathPrefix.new(path)
@@ -23,67 +23,67 @@ module Hanami
       end
 
       # @api private
-      # @since 0.1.0
+      # @since x.x.x
       def get(path, options = {}, &endpoint)
         super(@path.join(path), options, &endpoint)
       end
 
       # @api private
-      # @since 0.1.0
+      # @since x.x.x
       def post(path, options = {}, &endpoint)
         super(@path.join(path), options, &endpoint)
       end
 
       # @api private
-      # @since 0.1.0
+      # @since x.x.x
       def put(path, options = {}, &endpoint)
         super(@path.join(path), options, &endpoint)
       end
 
       # @api private
-      # @since 0.1.0
+      # @since x.x.x
       def patch(path, options = {}, &endpoint)
         super(@path.join(path), options, &endpoint)
       end
 
       # @api private
-      # @since 0.1.0
+      # @since x.x.x
       def delete(path, options = {}, &endpoint)
         super(@path.join(path), options, &endpoint)
       end
 
       # @api private
-      # @since 0.1.0
+      # @since x.x.x
       def trace(path, options = {}, &endpoint)
         super(@path.join(path), options, &endpoint)
       end
 
       # @api private
-      # @since 0.1.0
+      # @since x.x.x
       def options(path, options = {}, &endpoint)
         super(@path.join(path), options, &endpoint)
       end
 
       # @api private
-      # @since 0.1.0
+      # @since x.x.x
       def resource(name, options = {})
         super name, options.merge(prefix: @path.relative_join(options[:prefix]))
       end
 
       # @api private
-      # @since 0.1.0
+      # @since x.x.x
       def resources(name, options = {})
         super name, options.merge(prefix: @path.relative_join(options[:prefix]))
       end
 
       # @api private
-      # @since 0.1.0
+      # @since x.x.x
       def redirect(path, options = {}, &endpoint)
         super(@path.join(path), options.merge(to: @path.join(options[:to])), &endpoint)
       end
 
       # @api private
-      # @since 1.1.0
+      # @since x.x.x
       def mount(app, options)
         super(app, options.merge(at: @path.join(options[:at])))
       end
@@ -91,7 +91,7 @@ module Hanami
       # Supports nested prefix
       #
       # @api private
-      # @since 0.1.0
+      # @since x.x.x
       def prefix(path, &blk)
         self.class.new(self, path, &blk)
       end
