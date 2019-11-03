aebce3a3cd3a402152f65dfe98b008bff3bd2428
Tim Riley <tim@openmonkey.com>
2019-11-03 12:17:29 +1100

    Add evaluation context and pass as block argument when eval'ing routes

--- lib/hanami/router.rb
+++ lib/hanami/router.rb
@@ -243,7 +243,8 @@ module Hanami
     #   [200, {}, ["{:name=>\"LG\",:id=>\"1\"}"]]
     #
     # rubocop:disable Metrics/MethodLength
-    def initialize(scheme: "http", host: "localhost", port: 80, prefix: "", namespace: nil, configuration: nil, endpoint_resolver: Routing::Endpoint::Resolver.new, inflector: Dry::Inflector.new, not_found: NOT_FOUND, not_allowed: NOT_ALLOWED, &blk)
+    def initialize(context: nil, scheme: "http", host: "localhost", port: 80, prefix: "", namespace: nil, configuration: nil, endpoint_resolver: Routing::Endpoint::Resolver.new, inflector: Dry::Inflector.new, not_found: NOT_FOUND, not_allowed: NOT_ALLOWED, &blk)
+      @context           = context
       @routes            = []
       @namespace         = namespace
       @configuration     = configuration
@@ -254,7 +255,7 @@ module Hanami
       @inflector         = inflector
       @not_found         = not_found
       @not_allowed       = not_allowed
-      instance_eval(&blk) unless blk.nil?
+      instance_exec(context, &blk) if blk
       freeze
     end
     # rubocop:enable Metrics/MethodLength
