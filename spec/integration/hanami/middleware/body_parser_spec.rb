a28017a069fe9c7567247299c7594d4f446283c0
Peter Solnica <peter@solnica.online>
2022-10-14 06:44:39 +0200

    Simplify wording in the spec

--- spec/integration/hanami/middleware/body_parser_spec.rb
+++ spec/integration/hanami/middleware/body_parser_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe Hanami::Middleware::BodyParser do
   let(:app) { ->(_env) { [200, {}, "app"] } }
 
   describe ".new" do
-    it "accepts a parser identificator" do
+    it "accepts a parser name" do
       body_parser = Hanami::Middleware::BodyParser.new(app, :json)
 
       expect(body_parser.instance_variable_get("@parsers")["application/json"])
         .to be_instance_of(Hanami::Middleware::BodyParser::JsonParser)
     end
 
-    it "accepts a parser identificator with additional mime-type(s)" do
+    it "accepts a parser name with additional mime-type(s)" do
       body_parser = Hanami::Middleware::BodyParser.new(app, [json: "application/json+scim"])
 
       expect(body_parser.instance_variable_get("@parsers")["application/json+scim"])
