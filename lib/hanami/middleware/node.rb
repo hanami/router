0baac2bc9ae0d72f28cca832be4772eb6dca6742
Luca Guidi <me@lucaguidi.com>
2022-11-02 10:10:49 +0100

    Specs for `Hanami::Middleware::Node`

--- lib/hanami/middleware/node.rb
+++ lib/hanami/middleware/node.rb
@@ -5,52 +5,52 @@ module Hanami
     # Trie node to register scopes with custom Rack middleware
     #
     # @api private
-    # @since 0.1.1
+    # @since 2.0.0
     class Node
       # @api private
-      # @since 0.1.1
+      # @since 2.0.0
       attr_reader :app
 
       # @api private
-      # @since 0.1.1
+      # @since 2.0.0
       def initialize
         @app = nil
         @children = {}
       end
 
       # @api private
-      # @since 0.1.1
+      # @since 2.0.0
       def freeze
-        @children.each(&:freeze)
+        @children.freeze
         super
       end
 
       # @api private
-      # @since 0.1.1
+      # @since 2.0.0
       def put(segment)
         @children[segment] ||= self.class.new
       end
 
       # @api private
-      # @since 0.1.1
+      # @since 2.0.0
       def get(segment)
         @children.fetch(segment) { self if leaf? }
       end
 
       # @api private
-      # @since 0.1.1
+      # @since 2.0.0
       def app!(app)
         @app = app
       end
 
       # @api private
-      # @since 0.1.1
+      # @since 2.0.0
       def app?
-        @app
+        !@app.nil?
       end
 
       # @api private
-      # @since 0.1.1
+      # @since 2.0.0
       def leaf?
         @children.empty?
       end
