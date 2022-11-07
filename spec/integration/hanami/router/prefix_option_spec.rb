1056899c16a1cda79941b4bfb6eb2431f5e6040b
Luca Guidi <me@lucaguidi.com>
2022-11-07 10:19:12 +0100

    Ensure root path to work with prefix

--- spec/integration/hanami/router/prefix_option_spec.rb
+++ spec/integration/hanami/router/prefix_option_spec.rb
@@ -2,9 +2,10 @@
 
 RSpec.describe Hanami::Router do
   describe "with prefix option" do
-    let(:router) do
+    subject do
       e = endpoint
-      Hanami::Router.new(base_url: base_url, prefix: prefix) do
+      described_class.new(base_url: base_url, prefix: prefix) do
+        root             to: e
         get     "/home", to: e, as: :get_home
         post    "/home", to: e, as: :post_home
         put     "/home", to: e, as: :put_home
@@ -29,39 +30,50 @@ RSpec.describe Hanami::Router do
     let(:endpoint) { ->(*) { [200, {"Content-Length" => "4"}, ["home"]] } }
 
     it "generates relative URLs with prefix" do
-      expect(router.path(:get_home)).to eq("/admin/home")
-      expect(router.path(:post_home)).to eq("/admin/home")
-      expect(router.path(:put_home)).to eq("/admin/home")
-      expect(router.path(:patch_home)).to eq("/admin/home")
-      expect(router.path(:delete_home)).to eq("/admin/home")
-      expect(router.path(:trace_home)).to eq("/admin/home")
-      expect(router.path(:options_home)).to eq("/admin/home")
+      expect(subject.path(:root)).to eq("/admin")
 
-      expect(router.path(:get_admin)).to eq("/admin/admin")
-      expect(router.path(:new_admin)).to eq("/admin/admin/new")
-      expect(router.path(:create_admin)).to eq("/admin/admin")
-      expect(router.path(:edit_admin)).to eq("/admin/admin/edit")
-      expect(router.path(:put_admin)).to eq("/admin/admin")
+      expect(subject.path(:get_home)).to eq("/admin/home")
+      expect(subject.path(:post_home)).to eq("/admin/home")
+      expect(subject.path(:put_home)).to eq("/admin/home")
+      expect(subject.path(:patch_home)).to eq("/admin/home")
+      expect(subject.path(:delete_home)).to eq("/admin/home")
+      expect(subject.path(:trace_home)).to eq("/admin/home")
+      expect(subject.path(:options_home)).to eq("/admin/home")
 
-      expect(router.path(:dashboard_home)).to eq("/admin/dashboard/home")
+      expect(subject.path(:get_admin)).to eq("/admin/admin")
+      expect(subject.path(:new_admin)).to eq("/admin/admin/new")
+      expect(subject.path(:create_admin)).to eq("/admin/admin")
+      expect(subject.path(:edit_admin)).to eq("/admin/admin/edit")
+      expect(subject.path(:put_admin)).to eq("/admin/admin")
+
+      expect(subject.path(:dashboard_home)).to eq("/admin/dashboard/home")
     end
 
     it "generates absolute URLs with prefix" do
-      expect(router.url(:get_home)).to eq("https://hanami.test/admin/home")
-      expect(router.url(:post_home)).to eq("https://hanami.test/admin/home")
-      expect(router.url(:put_home)).to eq("https://hanami.test/admin/home")
-      expect(router.url(:patch_home)).to eq("https://hanami.test/admin/home")
-      expect(router.url(:delete_home)).to eq("https://hanami.test/admin/home")
-      expect(router.url(:trace_home)).to eq("https://hanami.test/admin/home")
-      expect(router.url(:options_home)).to eq("https://hanami.test/admin/home")
+      expect(subject.url(:root)).to eq("https://hanami.test/admin")
 
-      expect(router.url(:dashboard_home)).to eq("https://hanami.test/admin/dashboard/home")
+      expect(subject.url(:get_home)).to eq("https://hanami.test/admin/home")
+      expect(subject.url(:post_home)).to eq("https://hanami.test/admin/home")
+      expect(subject.url(:put_home)).to eq("https://hanami.test/admin/home")
+      expect(subject.url(:patch_home)).to eq("https://hanami.test/admin/home")
+      expect(subject.url(:delete_home)).to eq("https://hanami.test/admin/home")
+      expect(subject.url(:trace_home)).to eq("https://hanami.test/admin/home")
+      expect(subject.url(:options_home)).to eq("https://hanami.test/admin/home")
+
+      expect(subject.url(:dashboard_home)).to eq("https://hanami.test/admin/dashboard/home")
+    end
+
+    it "recognizes requests to root" do
+      env = Rack::MockRequest.env_for("/admin")
+      status, *_ = subject.call(env)
+
+      expect(status).to eq(200)
     end
 
     %w[GET POST PUT PATCH DELETE TRACE OPTIONS].each do |verb|
       it "recognizes requests (#{verb})" do
         env = Rack::MockRequest.env_for("/admin/home", method: verb)
-        status, _, body = router.call(env)
+        status, _, body = subject.call(env)
 
         expect(status).to eq(200)
         expect(body).to eq(["home"])
@@ -69,13 +81,13 @@ RSpec.describe Hanami::Router do
     end
 
     it "redirect works with prefix" do
-      router = Hanami::Router.new(prefix: prefix) do
+      subject = described_class.new(prefix: prefix) do
         redirect "/redirect", to: "/redirect_destination"
         get "/redirect_destination", to: ->(*) { [200, {}, ["Redirect destination!"]] }
       end
 
       env = Rack::MockRequest.env_for("/admin/redirect")
-      status, headers, = router.call(env)
+      status, headers, = subject.call(env)
 
       expect(status).to eq(301)
       expect(headers["Location"]).to eq("/admin/redirect_destination")
