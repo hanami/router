36185688789292cd35807c37d2370cd8e769a2cf
Damian C. Rossney <damian@rossney.net>
2024-08-31 19:59:29 -0400

    refactor Node class with new tests

---
+++ spec/unit/hanami/router/node_spec.rb
@@ -0,0 +1,116 @@
+# frozen_string_literal: true
+
+require "hanami/router/node"
+require "hanami/router/leaf"
+
+RSpec.describe Hanami::Router::Node do
+  describe "#initialize" do
+    it "returns a #{described_class} instance" do
+      expect(subject).to be_kind_of(described_class)
+    end
+  end
+
+  describe "#put" do
+    it "adds a node matching segment" do
+      segment = "foo"
+      subject.put(segment)
+    end
+  end
+
+  describe "#get" do
+    context "when segment is found" do
+      context "and segment is fixed" do
+        it "returns a node" do
+          segment = "foo"
+          subject.put(segment)
+
+          expect(subject.get(segment)).to be_kind_of(described_class)
+        end
+      end
+
+      context "and segment is variable" do
+        it "returns a node" do
+          dynamic_segment = ":foo"
+          subject.put(dynamic_segment)
+
+          expect(subject.get("bar")).to be_kind_of(described_class)
+        end
+      end
+    end
+
+    context "when segment is not found" do
+      it "returns nil" do
+        segment = "foo"
+        subject.put(segment)
+
+        expect(subject.get("bar")).to be_nil
+      end
+    end
+  end
+
+  describe "#leaf!" do
+    it "sets leaf data" do
+      route = "/route"
+      to = double("to")
+      constraints = {}
+      subject.leaf!(route, to, constraints)
+    end
+  end
+
+  describe "#match" do
+    context "when segment is fixed" do
+      context "and match not found" do
+        it "returns nil" do
+          segment = "foo"
+          route = "/foo"
+          to = double("to")
+          constraints = {}
+          path = "/bar"
+          subject.put(segment).leaf!(route, to, constraints)
+
+          expect(subject.get(segment).match(path)).to be_nil
+        end
+      end
+
+      context "and match is found" do
+        it "returns a Leaf object" do
+          segment = "foo"
+          route = "/foo"
+          to = double("to")
+          constraints = {}
+          subject.put(segment).leaf!(route, to, constraints)
+
+          expect(subject.get(segment).match(route)).to be_kind_of(Hanami::Router::Leaf)
+        end
+      end
+    end
+
+    context "when segment is variable" do
+      context "and match not found" do
+        it "returns nil" do
+          segment = ":foo"
+          route = "/:foo"
+          to = double("to")
+          constraints = { foo: :digit }
+          path = "/bar"
+          subject.put(segment).leaf!(route, to, constraints)
+
+          expect(subject.get(segment).match(path)).to be_nil
+        end
+      end
+
+      context "and match found" do
+        it "returns Leaf object" do
+          segment = ":foo"
+          route = "/:foo"
+          to = double("to")
+          constraints = { foo: :digit }
+          path = "/123"
+          subject.put(segment).leaf!(route, to, constraints)
+
+          expect(subject.get(segment).match(path)).to be_kind_of(Hanami::Router::Leaf)
+        end
+      end
+    end
+  end
+end
