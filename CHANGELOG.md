d129f253ab32490e5567ca755d56fd1a1053e8ca
Tim Riley <tim@riley.id.au>
2024-09-25 21:06:11 +1000

    Prepare for v2.2.0.beta2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 Rack compatible HTTP router for Ruby
 
+## v2.2.0.beta2 - 2024-09-25
+
 ## v2.2.0.beta1 - 2024-07-16
 
 ### Changed
--- spec/unit/hanami/router/version_spec.rb
+++ spec/unit/hanami/router/version_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe "Hanami::Router::VERSION" do
   it "exposes version" do
-    expect(Hanami::Router::VERSION).to eq("2.2.0.beta1")
+    expect(Hanami::Router::VERSION).to eq("2.2.0.beta2")
   end
 end
