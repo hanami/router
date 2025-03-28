6507dad6123911ec08f62f4b3e4d94f48a8ba8da
Kyle Plump <kplump1395@gmail.com>
2025-03-28 16:44:20 -0400

    Update lib/hanami/middleware/body_parser.rb
    Co-authored-by: Tim Riley <tim@riley.id.au>

--- lib/hanami/middleware/body_parser.rb
+++ lib/hanami/middleware/body_parser.rb
@@ -42,7 +42,8 @@ module Hanami
         body = env[RACK_INPUT]&.read
         return @app.call(env) if body.nil? || body.empty?
 
-        Rack::RewindableInput.new(env[RACK_INPUT]).rewind # somebody might try to read this stream
+        # Somebody might try to read this stream
+        Rack::RewindableInput.new(env[RACK_INPUT]).rewind 
 
         if (parser = @parsers[media_type(env)])
           env[Router::ROUTER_PARSED_BODY] = parser.parse(body, env)
