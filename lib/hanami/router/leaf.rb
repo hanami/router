891d7528a8fbd694033438f8c765eec550370864
Damian C. Rossney <damian@rossney.net>
2025-03-30 19:35:30 -0400

    implement basic regexp constraints

--- lib/hanami/router/leaf.rb
+++ lib/hanami/router/leaf.rb
@@ -14,7 +14,7 @@ module Hanami
       # @api private
       # @since 2.2.0
       def initialize(param_keys, to, constraints)
-        @param_keys = param_keys.map { |key| key[1..] }.freeze
+        @param_keys = param_keys.map { |key| key.delete_prefix(":") }.freeze
         @to = to
         @constraints = constraints
         @params = nil
@@ -23,7 +23,18 @@ module Hanami
       # @api private
       # @since 2.2.0
       def match(param_values)
+        return false if param_values.empty?
+
         @params = @param_keys.zip(param_values).to_h
+
+        return true if @constraints.empty?
+
+        @constraints.all? do |key, constraint|
+
+          match = constraint.match(@params[key.to_s])
+
+          match && (match.string == match[0])
+        end
       end
     end
   end
