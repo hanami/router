ecf0f93946f1234ce7af8819518279fb03ff7742
Damian C. Rossney <damian@rossney.net>
2025-04-15 08:00:38 -0400

    refactor unit tests for node to match updated node interface

--- spec/unit/hanami/router/node_spec.rb
+++ spec/unit/hanami/router/node_spec.rb
@@ -6,7 +6,33 @@ require "hanami/router/leaf"
 RSpec.describe Hanami::Router::Node do
   describe "#initialize" do
     it "returns a #{described_class} instance" do
-      expect(subject).to be_kind_of(described_class)
+      result = subject
+
+      expect(result).to be_kind_of(described_class)
+    end
+  end
+
+  describe "#put" do
+    context "when segment is fixed" do
+      it "does not update param_keys" do
+        segment = "foo"
+        param_keys = []
+
+        subject.put(segment, param_keys)
+
+        expect(param_keys).to be_empty
+      end
+    end
+
+    context "when segment is variable" do
+      it "updates param_keys" do
+        variable_segment = ":foo"
+        param_keys = []
+
+        subject.put(variable_segment, param_keys)
+
+        expect(param_keys).to include(":foo")
+      end
     end
   end
 
@@ -20,19 +46,51 @@ RSpec.describe Hanami::Router::Node do
 
           subject.put(segment, param_keys)
 
-          expect(subject.get(segment, param_values)).to be_kind_of(described_class)
+          result = subject.get(segment, param_values)
+
+          expect(result).to be_kind_of(described_class)
+        end
+
+        it "does not update param_values" do
+          segment = "foo"
+          param_keys = []
+          param_values = []
+
+          subject.put(segment, param_keys)
+          subject.get(segment, param_values)
+
+          result = param_values
+
+          expect(result).to be_empty
         end
       end
 
       context "and segment is variable" do
         it "returns a node" do
-          dynamic_segment = ":foo"
+          variable_segment = ":foo"
           param_keys = []
+          path_segment = "bar"
           param_values = []
 
-          subject.put(dynamic_segment, param_keys)
+          subject.put(variable_segment, param_keys)
 
-          expect(subject.get("bar", param_values)).to be_kind_of(described_class)
+          result = subject.get(path_segment, param_values)
+
+          expect(result).to be_kind_of(described_class)
+        end
+
+        it "update param_values" do
+          variable_segment = ":foo"
+          param_keys = []
+          path_segment = "bar"
+          param_values = []
+
+          subject.put(variable_segment, param_keys)
+          subject.get(path_segment, param_values)
+
+          result = param_values
+
+          expect(result).to include("bar")
         end
       end
     end
@@ -41,80 +99,65 @@ RSpec.describe Hanami::Router::Node do
       it "returns nil" do
         segment = "foo"
         param_keys = []
+        path_segment = "bar"
         param_values = []
 
         subject.put(segment, param_keys)
 
-        expect(subject.get("bar", param_values)).to be_nil
+        result = subject.get(path_segment, param_values)
+
+        expect(result).to be_nil
+      end
+    end
+  end
+
+  describe "#leaf!" do
+    context "when called" do
+      it "returns a Leaf object" do
+        segment = "foo"
+        to = "target action"
+        constraints = {}
+        param_keys = []
+        param_values = []
+
+        subject.put(segment, param_keys).leaf!(param_keys, to, constraints)
+
+        result = subject.get(segment, param_values).match(param_values)
+
+        expect(result).to be_kind_of(Hanami::Router::Leaf)
       end
     end
   end
 
   describe "#match" do
-    context "when segment is fixed" do
-      context "and match not found" do
-        it "returns nil" do
-          segment = "foo"
-          route = "/foo"
-          to = double("to")
-          constraints = {}
-          path = "/bar"
-          param_keys = []
-          param_values = []
+    context "when @leaves collection is empty" do
+      it "returns nil" do
+        segment = "foo"
+        param_keys = []
+        param_values = []
 
-          subject.put(segment, param_keys).leaf!(param_keys, to, constraints)
+        subject.put(segment, param_keys)
 
-          expect(subject.get(segment, param_values).match(param_values)).to be_nil
-        end
-      end
+        result = subject.get(segment, param_values).match(param_values)
 
-      context "and match is found" do
-        it "returns a Leaf object" do
-          segment = "foo"
-          route = "/foo"
-          to = double("to")
-          constraints = {}
-          param_keys = []
-          param_values = []
-
-          subject.put(segment, param_keys).leaf!(param_keys, to, constraints)
-
-          expect(subject.get(segment, param_values).match(param_values)).to be_kind_of(Hanami::Router::Leaf)
-        end
+        expect(result).to be_nil
       end
     end
 
-    context "when segment is variable" do
-      context "and match not found" do
-        it "returns nil" do
-          segment = ":foo"
-          route = "/:foo"
-          to = double("to")
-          constraints = {foo: :digit}
-          path = "/bar"
-          param_keys = []
-          param_values = []
+    context "when @leaves collection contains a match" do
+      it "returns the matching leaf" do
+        segment = "foo"
+        to = "target action"
+        constraints = {}
+        param_keys = []
+        param_values = []
 
-          subject.put(segment, param_keys).leaf!(param_keys, to, constraints)
+        subject.put(segment, param_keys).leaf!(param_keys, to, constraints)
+        leaf = subject.get(segment, param_values).match(param_values)
 
-          expect(subject.get(segment, param_values).match(param_values)).to be_nil
-        end
-      end
+        result = leaf.to
 
-      context "and match found" do
-        it "returns Leaf object" do
-          segment = ":foo"
-          route = "/:foo"
-          to = double("to")
-          constraints = {foo: :digit}
-          path = "/123"
-          param_keys = []
-          param_values = []
-
-          subject.put(segment, param_keys).leaf!(param_keys, to, constraints)
-
-          expect(subject.get(segment, param_values).match(param_values)).to be_kind_of(Hanami::Router::Leaf)
-        end
+        expect(result).to be("target action")
       end
     end
   end
